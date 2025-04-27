1aa017e3ae4a60c5d11e08f35f665ef5c2055790
jorgeramirezcarrasco <jorgerc93@msn.com>
2025-04-27 21:30:29 +0300

    feat: add vitest config

---
+++ vitest.config.js
@@ -0,0 +1,9 @@
+// vitest.config.js
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // any pattern here is **excluded**
+    exclude: ['e2e/**', 'node_modules/**'],
+  },
+});
